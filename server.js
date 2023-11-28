ef68d8282b3dfb32831ba4a407147c5dc0d44cea
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-11-28 16:01:37 +0100

    printing out the mesages array, by looping through the array in the messages array

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-];
-
-
 //Loading express and rendering routing pages
 const express = require("express");
 const app = express();
+
+//messages array!
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    },
+    {
+        text: "POTATO",
+        user: "Zijlboot",
+        added: new Date()
+    }
+];
 
 //using a view engine to render htmls pages via express!
 app.set('view engine', 'ejs');
@@ -22,7 +27,7 @@ app.set('view engine', 'ejs');
 //route form homepage
 app.get("/", (req, res, next) => {
     console.log("Home Page entered");
-    res.render('index', {text: 'world'});
+    res.render('index', { title: "Mini Messageboard", messages: messages })
 })
 
 //route for new message

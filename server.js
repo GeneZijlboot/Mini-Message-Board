10c04917aa045f071a452bb6c610c89458e2ab90
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-11-28 15:44:59 +0100

    changes made to the setup + array message added

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const express = require("express");
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+];
 
+
+//Loading express and rendering routing pages
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs'); //using a view engine to render htmls pages via express!
+//using a view engine to render htmls pages via express!
+app.set('view engine', 'ejs');
 
+//route form homepage
 app.get("/", (req, res, next) => {
     console.log("Home Page entered");
     res.render('index', {text: 'world'});
 })
 
+//route for new message
+app.get("/new", (req, res, next) => {
+    console.log("new Page entered");
+    res.render("new")
+})
+//renders server on port 3000
 app.listen(3000);
-
-const formRouter = require('./routes/form')
-
-app.use('/', formRouter);

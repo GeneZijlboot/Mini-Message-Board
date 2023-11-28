520f231ff160f10b03ab0ebe63e7530086446088
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-11-28 14:27:35 +0100

    adding a router for the form page

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express();
 app.set('view engine', 'ejs'); //using a view engine to render htmls pages via express!
 
 app.get("/", (req, res, next) => {
-    console.log("Running");
-    res.render('index');
+    console.log("Home Page entered");
+    res.render('index', {text: 'world'});
 })
 
 app.listen(3000);
+
+const formRouter = require('./routes/form')
+
+app.use('/', formRouter);

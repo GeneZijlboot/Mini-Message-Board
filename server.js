e373099687a945d45b12ce5e4800e5878a364bb4
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-11-30 16:15:01 +0100

    Adding a post request for the form data, and pushing the new data to the 'messages' array. for it to be displayed.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express();
 
+//using a view engine to render htmls pages via express!
+app.set('view engine', 'ejs');
+
 //messages array (replacement for mongoDB atm!)
 const messages = [
     {
@@ -21,19 +24,28 @@ const messages = [
     }
 ];
 
-//using a view engine to render htmls pages via express!
-app.set('view engine', 'ejs');
+// Middleware to parse the request body
+app.use(express.urlencoded({ extended: true }));
 
 //route form homepage
 app.get("/", (req, res, next) => {
     console.log("Home Page entered");
     res.render('index', { title: "Mini Messageboard", messages: messages })
-})
+});
 
 //route for new message
 app.get("/new", (req, res, next) => {
     console.log("new Page entered");
     res.render("new")
-})
+});
+
+app.post("/new", (req, res, next) => {
+    console.log(req.body); // Log the entire req.body object to the console
+    const { author, message } = req.body;
+    messages.push({text: author, user: message, added: new Date()});
+    console.log("Message added:", { author, message });
+    res.redirect("/");
+});
+
 //renders server on port 3000
 app.listen(3000);
